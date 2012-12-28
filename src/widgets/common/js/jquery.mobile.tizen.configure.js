40a698b9e8c3ab0ce9e1a5be2a52912c655fd7c1
hjnim.kim <hjnim.kim@samsung.com>
2012-12-28 15:07:20 +0900

    Button: data-corners attribute has been added
    JQM data-corners value has been changed from true to false
    and applied data-corners attribute
    
    Change-Id: Ibc543663c5495c65141449a3a3e300903e718327

--- src/widgets/common/js/jquery.mobile.tizen.configure.js
+++ src/widgets/common/js/jquery.mobile.tizen.configure.js
@@ -7,5 +7,6 @@
 	/* depth transition */
 	$.mobile.transitionHandlers.depth = $.mobile.transitionHandlers.simultaneous;
 	$.mobile.transitionFallbacks.depth = "fade";
-
+	/* Button data-corners default value */
+	$.fn.buttonMarkup.defaults.corners = false;
 })( jQuery, this );
