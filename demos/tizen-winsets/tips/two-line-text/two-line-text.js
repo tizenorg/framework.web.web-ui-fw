f411715c8031e4fb66db0c2435af4e5670cd9052
Jinhyuk Jun <jinhyuk.jun@samsung.com>
2012-12-28 15:01:04 +0900

    tizen demo : bug fix
    reposition 2line-text button, vertical popup position fix, more popup demo bug fix
    
    Change-Id: I3a69b7d6dda19b41f69368c7f74178201418972a

--- demos/tizen-winsets/tips/two-line-text/two-line-text.js
+++ demos/tizen-winsets/tips/two-line-text/two-line-text.js
@@ -1,8 +1,8 @@
 $( document ).bind( "pagebeforeshow", function( e ) {
 	if( $( "#textposition" ).length ) {
-		$( "#textposition" ).css( "font-size", "12px" );
-		$( "#textposition" ).find( "span" ).css( "height", "32px" );
-		$( "#textposition" ).find( "span" ).css("padding-top", "4px");
-		$( "#textposition" ).find( "span" ).css("padding-bottom", "4px");
+		$( "#textposition .ui-btn-text" ).css( "font-size", "12px" );
+		$( "#textposition" ).find( "span" ).css( "line-height", "10px" );
+		$( "#textposition" ).find( "span" ).css("padding-top", "2px");
+		$( "#textposition" ).find( "span" ).css("padding-bottom", "2px");
 	}
 });
