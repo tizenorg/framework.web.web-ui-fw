c7ca4624346f082f74dcfb97933ed27849b3ddf0
Minkyu Kang <mk7.kang@samsung.com>
2013-01-04 14:50:58 +0900

    unit-test: gallery: add new APIs
    add empty, length, value functions
    
    Change-Id: If3a9b6e3c0dc6e1afc77b693db20d797a3851840

--- tests/unit-tests/gallery/gallery-tests.js
+++ tests/unit-tests/gallery/gallery-tests.js
@@ -10,7 +10,7 @@
 	var unit_gallery = function ( widget, count ) {
 		var imagesldier,
 			refresh = function ( widget ) {
-				widget.gallery("refresh");
+				widget.gallery("refresh", "1");
 				return widget.find(".ui-gallery-bg");
 			};
 
@@ -23,16 +23,27 @@
 		/* Initialize */
 		equal( gallery.length, count, "Initialize" );
 
-		/* API: add */
-		widget.gallery("add", "05.jpg");
-		widget.gallery("add", "06.jpg");
-		gallery = refresh( widget );
-		equal( gallery.length, count + 2, "API: add" );
+		/* API: length */
+		equal( widget.gallery("length"), count, "API: length" );
 
 		/* API: del */
 		widget.gallery("remove");
 		gallery = refresh( widget );
-		equal( gallery.length, count + 1, "API: del" );
+		equal( gallery.length, count - 1, "API: del" );
+
+		/* API: add */
+		widget.gallery("add", "05.jpg");
+		widget.gallery("add", "06.jpg");
+		gallery = refresh( widget );
+		equal( gallery.length, count + 1, "API: add" );
+
+		/* API: length */
+		equal( widget.gallery("value"), 1, "API: value" );
+
+		/* API: empty */
+		widget.gallery("empty");
+		gallery = refresh( widget );
+		equal( gallery.length, 0, "API: empty" );
 	};
 
 	test( "gallery", function () {
