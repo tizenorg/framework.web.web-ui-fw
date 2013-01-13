ed11b368465ab2dd182efb8c2fb97c3b68f46d30
hjnim.kim <hjnim.kim@samsung.com>
2013-01-13 14:35:29 +0900

    Demo: Context popup close method has been changed
    Context popup close method has been changed
    ( css close method not using anymore, it was replaced 'data-rel="back"' )
    
    Change-Id: I31da4d4be83f12ce674b47b8e9358e7699dc5ec2

--- demos/tizen-winsets/widgets/ctxpopup.js
+++ demos/tizen-winsets/widgets/ctxpopup.js
@@ -1,17 +1,10 @@
 $("#pop_js").live("vclick", function ( e ) {
-	if ( $(e.target).is(".ui-btn-ctxpopup-close") ) {
-		$(this).popup("close");
-	}
 	if ( $(e.target).is("#ctxpopup_update") ) {
 		$("#btn_js").text("Peekaboo!");
 		$("#btn_js").buttonMarkup("refresh");
 	}
 });
 
-var closeTest = function()
-{
-	alert('test');
-}
 var popupTest = function()
 {
 	$("#pop_text_only").popup("open");
