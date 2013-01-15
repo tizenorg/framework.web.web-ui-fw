dc824b6e911d22e68ed00e4e0388ad0c2b29ef59
Minkyu Kang <mk7.kang@samsung.com>
2013-01-15 13:46:58 +0900

    configure: set TIZEN default configures
    Set TIZEN default values at configure file instead of JQM patch
    
    Change-Id: I153efc1d9a93e1001ecc56fb43349345537acfb0

--- src/widgets/common/js/jquery.mobile.tizen.configure.js
+++ src/widgets/common/js/jquery.mobile.tizen.configure.js
@@ -4,9 +4,17 @@
 
 ( function( $, window, undefined ) {
 
+	/* set default transition */
+	$.mobile.defaultPageTransition = "none";
+
 	/* depth transition */
 	$.mobile.transitionHandlers.depth = $.mobile.transitionHandlers.simultaneous;
 	$.mobile.transitionFallbacks.depth = "fade";
+
 	/* Button data-corners default value */
 	$.fn.buttonMarkup.defaults.corners = false;
+
+	/* button hover delay */
+	$.mobile.buttonMarkup.hoverDelay = 0;
+
 })( jQuery, this );
