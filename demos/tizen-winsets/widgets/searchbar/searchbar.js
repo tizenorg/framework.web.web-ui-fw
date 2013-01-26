cbd48a59faae9c25573285be0fcf68b1961c2459
Youmin Ha <youmin.ha@samsung.com>
2013-01-26 13:28:14 +0900

    demo: fix searchbar demo to use different IDs
    The searchbar demos have same IDs for different elements, and the
    searchbar-button sample does not work. Fixed the bug to work properly.
    
    Change-Id: I46f7ac80f1ab2fea10a07bcffe169322b0bdc001

--- demos/tizen-winsets/widgets/searchbar/searchbar.js
+++ demos/tizen-winsets/widgets/searchbar/searchbar.js
@@ -1,24 +1,41 @@
-$( "#searchbar-demo-page" ).bind( "pageshow", function(){
-	$( "#search1" ).bind( "input change", function(){
-		var regEx = "";
+( function ( ) {
 
-		regEx = ".*" + $( "#search1" ).val();
+function setSearchbar( pageID ) {
 
-		$("#searchbar-content p").each(function(){
-			if ( $( this ).text().search(new RegExp(regEx)) != -1) {
-				$( this ).show();
-			}
-			else {
-				$( this ).hide();
-			}
+	$( "#" + pageID ).bind( "pageshow", function( ev ){
+		var page = ev.target;
+
+		$( "#" + pageID + "-search" ).bind( "input change", function( ev ){
+			var regEx,
+				sbar = ev.target,
+				content = $( page ).children(":jqmData(role='content')")[0];
+
+			regEx = ".*" + $( sbar ).val();
+
+			$( content ).find( "p" ).each( function( ){
+				if ( $( this ).text().search(new RegExp(regEx)) != -1) {
+					$( this ).show();
+				}
+				else {
+					$( this ).hide();
+				}
+			});
+
+			$( content ).scrollview("scrollTo", 0, 0, 0);
 		});
-
-		$("#searchbar-content").scrollview("scrollTo", 0, 0, 0);
 	});
-});
+}
+
+setSearchbar("searchbar-demo-page-normal");
+setSearchbar("searchbar-demo-page-cancel");
+setSearchbar("searchbar-demo-page-button");
 
 $( "#searchbar-demo-page-button" ).bind( "pageshow", function(){
 	$( ".ui-btn-search-front-icon" ).bind( "vclick", function(){
 		window.alert( "front button in searchbar pressed!" );
 	});
 });
+
+} ( ) );
+
+
