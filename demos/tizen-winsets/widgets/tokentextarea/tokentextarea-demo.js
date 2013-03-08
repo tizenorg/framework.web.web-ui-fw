8d2fd0c90a6540037be4d1a3e577611642fc68e5
Przemyslaw Ciezkowski <p.ciezkowski@samsung.com>
2013-03-08 16:49:53 +0100

    Move tokentextarea demo to external page.
    Change-Id: Ie92c45c82085db877510968bc955a5665a0390c0

--- demos/tizen-winsets/widgets/tokentextarea/tokentextarea-demo.js
+++ demos/tizen-winsets/widgets/tokentextarea/tokentextarea-demo.js
@@ -1,61 +1,61 @@
 ( function ( $, window ) {
-	$( document ).ready( function () {
-		$( "#MBTaddItemTest" ).click( function () {
+	$(document).one("pagecreate", "#tokentextarea", function () {
+		$( "#MBTaddItemTest" ).on( "click", function () {
 			$( ":jqmData(role='tokentextarea')" ).tokentextarea( "add", "additem" );
 		});
 
-		$( "#MBTremoveItemTest" ).click( function () {
+		$( "#MBTremoveItemTest" ).on( "click", function () {
 			$( ":jqmData(role='tokentextarea')" ).tokentextarea( "remove", 0 );
 		});
 
-		$( "#MBTinputTextTest" ).click( function () {
+		$( "#MBTinputTextTest" ).on( "click", function () {
 			$( ":jqmData(role='tokentextarea')" ).tokentextarea( "inputText", "Hello~~~" );
 		});
 
-		$( "#MBTgetInputTextTest" ).click( function () {
+		$( "#MBTgetInputTextTest" ).on( "click", function () {
 			var input = $( ":jqmData(role='tokentextarea')" ).tokentextarea( "inputText" );
 			window.alert( "input String : " + input );
 		});
 
-		$( "#MBTremoveAllItemTest" ).click( function () {
+		$( "#MBTremoveAllItemTest" ).on( "click", function () {
 			$( ":jqmData(role='tokentextarea')" ).tokentextarea( "remove" );
 			$( ":jqmData(role='tokentextarea')" ).parents( ".ui-scrollview-view" ).parent().scrollview( "scrollTo", 0, 0 );
 		});
 
-		$( "#MBTgetSelectedItemTest" ).click( function () {
+		$( "#MBTgetSelectedItemTest" ).on( "click", function () {
 			var content = $( ":jqmData(role='tokentextarea')" ).tokentextarea( "select" );
 			window.alert( "Select content : " + content );
 		});
 
-		$( "#MBTselectItemTest" ).click( function () {
+		$( "#MBTselectItemTest" ).on( "click", function () {
 			$( ":jqmData(role='tokentextarea')" ).tokentextarea( "select", 0 );
 		});
 
-		$( "#MBTlengthTest" ).click( function () {
+		$( "#MBTlengthTest" ).on( "click", function () {
 			var length = $( ":jqmData(role='tokentextarea')" ).tokentextarea( "length" );
 			window.alert( "length : " + length );
 		});
 
-		$( "#MBTfocusInTest" ).click( function () {
+		$( "#MBTfocusInTest" ).on( "click", function () {
 			$( ":jqmData(role='tokentextarea')" ).tokentextarea( "focusIn", 0 );
 			$( ":jqmData(role='tokentextarea')" ).parents( ".ui-scrollview-view" ).parent().scrollview( "scrollTo", 0, 0 );
 		});
 
-		$( "#MBTfocusOutTest" ).click( function () {
+		$( "#MBTfocusOutTest" ).on( "click", function () {
 			$( ":jqmData(role='tokentextarea')" ).tokentextarea( "focusOut", 0 );
 			$( ":jqmData(role='tokentextarea')" ).parents( ".ui-scrollview-view" ).parent().scrollview( "scrollTo", 0, 0 );
 		});
 
-		$( "#MBTdestroyTest" ).click( function () {
+		$( "#MBTdestroyTest" ).on( "click", function () {
 			$( ":jqmData(role='tokentextarea')" ).tokentextarea( "destroy" );
 		});
 
-		$( "#contentList a" ).click( function () {
+		$( "#contentList a" ).on( "click", function () {
 			var arg = $( this ).text();
 			$( ":jqmData(role='tokentextarea')" ).tokentextarea( "add", arg );
 		});
 
-		$( "#cancelBtn" ).click( function () {
+		$( "#cancelBtn" ).on( "click", function () {
 			$.mobile.changePage( "#tokentextarea", {
 				transition: "reverse slide",
 				reverse: false,
