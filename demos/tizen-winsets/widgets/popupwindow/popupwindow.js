f131b4a13e220007406685bd7e4bb34141c84b1b
hjnim.kim <hjnim.kim@samsung.com>
2013-04-15 17:26:12 +0900

    Tizenwinset: added return false in popup sample
    added return false in popup sample.
    ( reopen the popup issue )
    N_SE-34406
    
    Change-Id: Ie4d21789b461144c382cd50d69cc9d7c99fbf041

--- demos/tizen-winsets/widgets/popupwindow/popupwindow.js
+++ demos/tizen-winsets/widgets/popupwindow/popupwindow.js
@@ -2,5 +2,6 @@ $( document ).one( "pageinit", "#popupwindow-demo", function () {
 	$("#btn_textbox_popup_cancel").on("vclick", function ( ev ) {
 		$("#textbox_popup").find("input").val("");
 		$("#textbox_popup").popup("close");
+		return false;
 	});
 });
