285fa57e9f3964c1c77888195d05422d2a3856cb
Jinhyuk Jun <jinhyuk.jun@samsung.com>
2013-04-16 00:36:52 +0900

    TizenWinset : remove landscape SIP on mode
    Change-Id: Iaadf77b4303ca906931b8f97831c6302a7f45269
    
    Conflicts:
    
    	demos/tizen-winsets/widgets/popupwindow/popupwindow.js

--- demos/tizen-winsets/widgets/popupwindow/popupwindow.js
+++ demos/tizen-winsets/widgets/popupwindow/popupwindow.js
@@ -5,3 +5,24 @@ $( document ).one( "pageinit", "#popupwindow-demo", function () {
 		return false;
 	});
 });
+
+function onSuccessPopupCallback ( ori ) {
+        if ( ori.status === "PORTRAIT_PRIMARY" || ori.status === "PORTRAIT_SECONDARY" ) {
+		$("#textbox_popup_landscape").popup("close");
+		$("#go_textbox_popup a").attr("href", "#textbox_popup");
+        }
+        else if ( ori.status === "LANDSCAPE_PRIMARY" || ori.status === "LANDSCAPE_SECONDARY" ) {
+		$("#textbox_popup").popup("close");
+		$("#go_textbox_popup a").attr("href", "#textbox_popup_landscape");
+        }
+
+}
+
+function onErrorPopupCallback ( error ) {
+        console.error( "An error occurred " + error.message );
+}
+
+if ( $.tizen.__tizen__.util.isMobileBrowser() ) {
+	tizen.systeminfo.addPropertyValueChangeListener( "DEVICE_ORIENTATION", onSuccessPopupCallback, onErrorPopupCallback );
+}
+
