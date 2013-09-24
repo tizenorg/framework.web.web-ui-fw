e90fd5596ca27878b8c399a4145e6893b3a29b1c
Youmin Ha <youmin.ha@samsung.com>
2013-09-24 13:41:03 +0900

    Fix checkbox whitespace bug for removing define()
    This patch fixes runtime error
    
    Change-Id: I0a84a52dbde79fef8d296d4524a33eeb6a8dc22d

--- src/js/widgets/jquery.mobile.tizen.checkbox.js
+++ src/js/widgets/jquery.mobile.tizen.checkbox.js
@@ -3,7 +3,7 @@
 * Copyright (c) 2010 - 2013 Samsung Electronics Co., Ltd.
 * License : MIT License V2
 */
- //>>excludeStart("jqmBuildExclude", pragmas.jqmBuildExclude);
+//>>excludeStart("jqmBuildExclude", pragmas.jqmBuildExclude);
 //>>description: Checkbox widget
 //>>label: Checkbox
 //>>group: Tizen:Widgets
