e36c47e35dcd5bbfb74bec784eb9b8b64ce3c6e6
Jinhyuk Jun <jinhyuk.jun@samsung.com>
2013-10-22 20:20:41 +0900

    Pinch : skip ratio setting when touch value is not valid (PLM P131021-02072)

--- demos/tizen-winsets/tips/pinch-event/pinch.js
+++ demos/tizen-winsets/tips/pinch-event/pinch.js
@@ -19,7 +19,7 @@ $( document ).one( "pageshow", "#pinch_page", function () {
 
 		ratio = get_ratio( p.ratio );
 
-		if ( current_ratio == ratio ) {
+		if ( current_ratio == ratio || !ratio) {
 			return;
 		}
 
@@ -36,7 +36,9 @@ $( document ).one( "pageshow", "#pinch_page", function () {
 	});
 
 	$("#pinch_demo").on( "pinchend", function ( e, p ) {
-		last_ratio = get_ratio( p.ratio );
+		if ( p.ratio ) {
+			last_ratio = get_ratio( p.ratio );
+		}
 	});
 
 	$( window ).bind( "galleryorientationchanged", function ( e ) {
